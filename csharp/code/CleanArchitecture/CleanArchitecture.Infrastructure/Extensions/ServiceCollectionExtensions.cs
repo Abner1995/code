dd90d3500b1a0dac5dc07307f6613ffd0762d43e
xuzizheng <wan19950504@gmail.com>
2024-12-07 15:56:37 +0800

    ✨ feat(Flexible Database connection ):
    Flexible Database connection

---
+++ csharp/code/CleanArchitecture/CleanArchitecture.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<RestaurantsDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("RestaurantsDb")));
+    }
+}
--- csharp/code/CleanArchitecture/CleanArchitecture.Infrastructure/Persistence/RestaurantsDbContext.cs
+++ csharp/code/CleanArchitecture/CleanArchitecture.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -5,13 +5,18 @@ namespace CleanArchitecture.Infrastructure.Persistence;
 
 internal class RestaurantsDbContext : DbContext
 {
+    public RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : base(options)
+    {
+
+    }
+
     internal DbSet<Restaurant> Restaurants { get; set; }
     internal DbSet<Dish> Dishes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Server=.;Database=restaurant;User ID=sa;Password=123456;TrustServerCertificate=True;");
-    }
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlServer("Server=.;Database=restaurant;User ID=sa;Password=123456;TrustServerCertificate=True;");
+    //}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
