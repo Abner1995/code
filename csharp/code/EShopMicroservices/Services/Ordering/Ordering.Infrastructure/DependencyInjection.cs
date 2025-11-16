44537954199c241bfc98ed7b6bddd1f1ef9f3322
xuzizheng <wan19950504@gmail.com>
2025-11-16 17:42:32 +0800

    feat(Ordering): 初始化订单应用层与基础设施层代码
    新增了 Ordering 应用层接口、DTO、异常类、命令处理器及验证逻辑，
    并配置 Entity Framework Core 数据库上下文与拦截器。包括：
    - 定义 `IApplicationDbContext` 接口及其实现
    - 添加 MediatR 配置以支持 CQRS 模式
    - 创建 Order 相关的 DTO 和命令处理逻辑（Create, Update, Delete）
    - 引入全局 using 简化命名空间引用
    - 注册数据库拦截器用于审计和领域事件发布
    
    此外，更新了依赖注入配置，确保服务正确注册和使用。

--- csharp/code/EShopMicroservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ csharp/code/EShopMicroservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastructure;
 
@@ -9,11 +11,18 @@ public static class DependencyInjection
     {
         var connectionString = configuration.GetConnectionString("Database");
 
-        services.AddDbContext<ApplicationDbContext>(options =>
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
         {
-            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            //报异常，需要在Ordering.Application注入IMediator mediator，所有要改写用上面的 
+            //options.AddInterceptors(new AuditableEntityInterceptor(), new DispatchDomainEventsInterceptor());
             options.UseSqlServer(connectionString);
         });
+
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
         return services;
     }
