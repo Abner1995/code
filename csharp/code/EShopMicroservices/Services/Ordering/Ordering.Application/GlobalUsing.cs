6c780280fb5d952fe2d8c00e38e72cc9c641b35d
xuzizheng <wan19950504@gmail.com>
2025-11-24 08:36:01 +0800

    feat(ordering): 添加订单管理API端点和分页支持
    新增了订单的创建、更新、删除、查询等API端点，使用Carter库进行路由配置。
    引入分页结果类PaginatedResult和分页请求记录PaginationRequest，
    用于支持订单列表的分页查询功能。同时添加了根据客户ID和订单名称查询订单的接口。
    
    此外，增加了订单相关的扩展方法用于DTO转换，并集成了MediatR、Mapster等库以支持命令查询职责分离模式。

--- csharp/code/EShopMicroservices/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ csharp/code/EShopMicroservices/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,7 +1,14 @@
-﻿global using BuildingBlocks.CQRS;
+﻿global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.EntityFrameworkCore;
+
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Pagination;
 
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Exceptions;
+global using Ordering.Application.Extensions;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Events;
---
+++ csharp/code/EShopMicroservices/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public class GetOrdersByNameHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery request, CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking()
+                .Where(o => o.OrderName.Value.Contains(request.Name))
+                .OrderBy(o => o.OrderName.Value)
+                .ToListAsync(cancellationToken);
+
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
+    }
+}
